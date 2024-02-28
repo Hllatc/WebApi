a60a90cffbb8573f84e7f61c5b30c62b031a8576
Hilal_atc <Hilal_atc@DESKTOP-F8GT9TS>
2024-02-28 17:40:16 +0100

    Add project files.

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public class BaseEntity<TId>  //id yi generic hale getirdik.
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UptadetDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+}
